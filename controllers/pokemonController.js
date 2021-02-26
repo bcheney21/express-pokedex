c1ec5fafebae466719efdd284c9c97b2922d4d9f
bcheney21 <brinn_cheney@icloud.comgit config --global user.email brinn_cheney@icloud.com>
2021-02-26 14:52:57 -0800

    able to add favorites

---
+++ controllers/pokemonController.js
@@ -0,0 +1,38 @@
+const router = require('express').Router()
+const db = require('../models')
+
+
+router.get('/', async (req, res) => {
+    try {
+        const pokemons = await db.pokemon.findAll({ raw: true })
+        // console.log(pokemons)
+
+        res.render('pokemon/index', { pokemons: pokemons })
+    } catch (err) {
+        console.log(err)
+        res.redirect('/')
+    }
+})
+
+router.post('/', async (req, res) => {
+    console.log(req.body)
+    try {
+        const [newPokemon, created] = await db.pokemon.findOrCreate({
+            where: { name: req.body.name }
+        })
+        // console.log(newPokemon)
+        // console.log(created)
+
+        res.redirect('/pokemons')
+    } catch (err) {
+        console.log(err)
+        res.redirect('/')
+    }
+})
+
+router.get('/:name', aysnc (req, res) => {
+    const pokeName = { name; req.body.name }
+    const pokeURL = 'http://pokeapi.co'
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,38 @@
 /* Required Modules and Variables */
+const express = require('express')
+const ejsLayouts = require('express-ejs-layouts')
+const rowdy = require('rowdy-logger')
+const axios = require('axios')
+const morgan = require('morgan')
 
+const app = express()
+const rowdyResults = rowdy.begin(app)
+const PORT = process.env.PORT || 3000
+
+/* Middleware and config */
+app.set('view engine', 'ejs')
+app.use(ejsLayouts)
+app.use(morgan('dev'))
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
 
 /* Controllers */
 
+app.use('/pokemons', require('./controllers/pokemonController'))
 
 /* Routes */
+app.get('/', async (req, res) => {
+    try {
+        const pokeURL = 'https://pokeapi.co/api/v2/pokemon?limit=151'
+        const response = await axios.get(pokeURL)
+        const pokemons = response.data.results
+        
+        res.render('index', { pokemons: pokemons })
+    } catch (err) {
+        console.log(err)
+        res.render('index', { pokemons: [] })
+    }
+})
+app.listen(PORT, () => {
+    rowdyResults.print()
+})
